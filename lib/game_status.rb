ccaa24b406d073bf20f14b92575ee93e3027fc1b
Megan Collier <meganraec@live.com>
2018-01-26 21:38:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -31,7 +31,7 @@ end
 def full?(board)
   if board.any? == " " || board.any? == ""
   return false
-  elsif board.all? == "X" || "O"
+elsif board.all? == "X" || "O"
   return true
 else false
 end
@@ -39,41 +39,6 @@ end
 
 #def draw?(board)
 
-=begin
-draw_board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-draw?(draw_board) #=> true
+#def over?
 
-x_diagonal_won = ["X", "O", "X", "O", "X", "O", "O", "O", "X"]
-draw?(x_diagonal_won) #=> false
-
-incomplete_board = ["X", " ", "X", " ", "X", " ", "O", "O", "X"]
-draw?(incomplete_board) #=> false
-
-end
-
-def over?
-=begin
-  draw_board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-over?(draw_board) #=> true
-
-won_board = ["X", "O", "X", "O", "X", "X", "O", "O", "X"]
-over?(won_board) #=> true
-
-inprogress_board = ["X", " ", "X", " ", "X", " ", "O", "O", " "]
-over?(inprogress_board) #=> false
-
-end
-
-def winner
-=begin
-  x_win_diagonal = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
-winner(x_win_diagonal) #=> "X"
-
-o_win_center_column = ["X", "O", " ", " ", "O", " ", " ", "O", "X"]
-winner(o_win_center_column) #=> "O"
-
-no_winner_board = ["X", "O", " ", " ", " ", " ", " ", "O", "X"]
-winner(no_winner_board) #=> nil
-
-end
-=end
+#def winner
