0af2b4ac1f549dea11fd5374bd62a621040e0262
Megan Collier <meganraec@live.com>
2018-01-26 21:30:17 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,14 +5,14 @@ end
 board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 
 WIN_COMBINATIONS = [
-  [0, 1, 2], #[0]
-  [3, 4, 5], #[1]
-  [6, 7, 8], #[2]
-  [0, 3, 6], #[3]
-  [1, 4, 7], #[4]
-  [2, 5, 8], #[5]
-  [0, 4, 8], #[6]
-  [2, 4, 6]  #[7]
+  [0, 1, 2], 
+  [3, 4, 5], 
+  [6, 7, 8], 
+  [0, 3, 6], 
+  [1, 4, 7], 
+  [2, 5, 8], 
+  [0, 4, 8], 
+  [2, 4, 6]  
 ]
 
 def won?(board)
@@ -28,35 +28,15 @@ def won?(board)
   return false
 end
 
-
-=begin
-    for each win_combination in WIN_COMBINATIONS
-  # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
-  # grab each index from the win_combination that composes a win.
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
-
-  position_1 = board[win_index_1] # load the value of the board at win_index_1
-  position_2 = board[win_index_2] # load the value of the board at win_index_2
-  position_3 = board[win_index_3] # load the value of the board at win_index_3
-
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination # return the win_combination indexes that won.
-  else
-    false
-  end
-end
-
-
 def full?(board)
   if board.all? == "X" || "O"
   return true
 elsif board.any? = " " || ""
+  return false
 else false
 end
 
-def draw?(board)
+#def draw?(board)
 
 =begin
 draw_board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
